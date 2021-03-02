7aa884aa59cac4dea74de342154c3ac12f570c21
LeeGiTaek <coolfamily77@gmail.com>
2021-03-02 18:41:49 +0900

    add XCTest

--- Sendbird-AssignmentTests/Sendbird_AssignmentTests.swift
+++ Sendbird-AssignmentTests/Sendbird_AssignmentTests.swift
@@ -29,5 +29,42 @@ class Sendbird_AssignmentTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    func testBookAPI () {
+        let bookUrl = "https://api.itbook.store/1.0/search/swift/1"
+        let encoded = bookUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let bookStoreURL = URL(string: encoded!)
+        var request = URLRequest(url: bookStoreURL!)
+       
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { data, response, error in
+        //    guard let data = data, error == nil else { return }
+            guard let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode == 200 else { return }
+            XCTAssertEqual(httpStatus.statusCode, 200)
+            if httpStatus.statusCode != 200 {
+                XCTFail("Status code: \(httpStatus.statusCode)")
+            }
+        }.resume()
+    }
+    
+    func testDetailInfo() {
+        let bookUrl = "https://api.itbook.store/1.0/books/9780983066989"
+        let encoded = bookUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let bookStoreURL = URL(string: encoded!)
+        var request = URLRequest(url: bookStoreURL!)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode == 200 else { return }
+            XCTAssertEqual(httpStatus.statusCode, 200)
+            if httpStatus.statusCode != 200 {
+                XCTFail("Status code: \(httpStatus.statusCode)")
+            }
+            
+        }.resume()
+    }
+    
+    
+   
+    
+    
 }
